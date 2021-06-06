d90d177bf559a9112165900f3dfa7cccf8dcc0fb
June P <TheyAreJune@gmail.com>
2021-06-05 23:02:38 -0400

    Added pixel-by-pixel capabilities, testing renderer.

--- Program.cs
+++ Program.cs
@@ -4,13 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
 namespace FastMandelbrotGenTesting
 {
     class RoughDraft
     {
+        static int resx = 2560;
+        static int resy = 1440;
+        
         static void Main(string[] args)
         {
+            using (Image<Rgba32> image = new Image<Rgba32>(resx, resy))
+            {
+                //Define Other Variables
+                float a;
+                float b;
+                float atemp;
+                int iter;
+                int max_iter = 64; //ITERATION COUNT. VERY IMPORTANT!!
 
+                // Color Switcher
+                float grayscale;
+
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Span<Rgba32> pixelRowSpan = image.GetPixelRowSpan(y);
+                    for (int x = 0; x < image.Width; x++)
+                    {
+                        /*
+                        *  if (Math.Round(Math.Sin(x) * resy, 0) == y)
+                        *  {
+                        *      pixelRowSpan[x] = new Rgba32(255.0f, 255.0f, 255.0f, 255.0f);
+                        *  }
+                        *  else
+                        *  {
+                        *      pixelRowSpan[x] = new Rgba32(0.0f, 0.0f, 0.0f, 255.0f);
+                        *  }
+                        */
+
+                        a = 0.0f;
+                        b = 0.0f;
+                        iter = 0;
+
+                        while ((a * a) + (b * b) <= 2 * 2 & iter <= max_iter)
+                        {
+                            atemp = (a * a) - (b * b) + x;
+                            b = 2 * a * b + y;
+                            a = atemp;
+                            iter++;
+                        }
+
+
+                        grayscale = iter / 255;
+                        // Output pixel with appropriate color
+                        pixelRowSpan[x] = new Rgba32(grayscale, grayscale, grayscale, 255.0f);
+
+                    }
+                }   
+                image.Save("bigtest.png");
+            }
         }
     }
 }
